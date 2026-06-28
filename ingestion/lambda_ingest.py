047f295b52486e96e4abe8e2d44c5820ee1f4e21
Mihir Ganguli <mgangul@gmu.edu>
2026-06-28 13:41:05 -0400

    separated flight dictionaries line by line when writing flight data to s3, Athena wasn't able to query properly

--- ingestion/lambda_ingest.py
+++ ingestion/lambda_ingest.py
@@ -65,22 +65,26 @@ def lambda_handler(event, context):
 
     s3_client = boto3.client("s3")
 
+    
+
     s3_client.put_object(
         Bucket="mihir-opensky-bucket",
         Key=f"raw/all_flights/{datetime.now().strftime('%Y-%m-%d-%H-%M')}.json",
-        Body=json.dumps(all_flights)
+        Body='\n'.join(json.dumps(flight) for flight in all_flights) #writes line by line to s3 instead of just one line
     )
 
     s3_client.put_object(
         Bucket="mihir-opensky-bucket",
         Key=f"raw/arrivals/{yesterday.strftime('%Y-%m-%d')}.json",
-        Body=json.dumps(arrivals)
+         #each airport contains a list of flight items, loop through and put each flight dictionary on separate line
+        Body= '\n'.join(json.dumps(flight) for  airport in arrivals for flight in airport)
     )
 
     s3_client.put_object(
         Bucket="mihir-opensky-bucket",
         Key=f"raw/departures/{yesterday.strftime('%Y-%m-%d')}.json",
-        Body=json.dumps(departures)
+        #each airport contains a list of flight items, loop through and put each flight dictionary on separate line
+        Body= '\n'.join(json.dumps(flight) for  airport in arrivals for flight in airport)
         )
     
 
